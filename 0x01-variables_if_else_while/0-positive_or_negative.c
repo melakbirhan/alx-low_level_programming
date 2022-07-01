7934afc57394cbf61614911a9e03490267f3787f
melakbirhan <melakbirhan@cbe.com.et>
2022-07-01 03:23:33 -0700

    0-positive_or_negative.c is commited

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,11 +2,9 @@
  * File: 0-positive_or_negative.c
  * Auth: Brennan D Baraban
  */
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
-
 /**
  * main - Prints a random number and states whether
  *        it is positive, negative, or zero.
@@ -16,10 +14,8 @@
 int main(void)
 {
 	int n;
-
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-
 	if (n > 0)
 		printf("%d is positive\n", n);
 	else if (n < 0)
