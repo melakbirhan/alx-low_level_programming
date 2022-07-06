19fe6b821d2cb19e607972b2d827a13d42616a9d
melakbirhan <melakbirhan@cbe.com.et>
2022-07-05 21:45:04 -0700

    Main

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 int _putchar (char char1);
 void print_alphabet(void);
 void print_alphabet_x10(void);
@@ -11,4 +12,4 @@ void jack_bauer(void);
 void times_table(void);
 int add(int, int);
 void print_to_98(int n);
-void print_times_table(int n)
+void print_times_table(int n);
