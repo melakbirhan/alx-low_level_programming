357e3130ead56c9c312cce7b984c6a0915ec7953
melakbirhan <melakbirhan@cbe.com.et>
2022-07-11 04:04:17 -0700

    main.h added

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,10 +1,3 @@
-/**
- * File: main.h
- * Auth: Helina Belete
- * Desc: Header file containing prototypes for all functions
- *       used in the 0x05-pointers_arrays_strings directory.
- */
-
 int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
