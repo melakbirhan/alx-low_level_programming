7a1d95b6a079026fbdfb0a6d9d23ec6803d953bf
melakbirhan <melakbirhan@cbe.com.et>
2022-07-18 15:18:37 +0300

    0-memset.c changed

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -1,17 +1,10 @@
 /**
-
  * _memset - fills memory with a constant byte
-
  * @s: memory space to be filled
-
  * @b: byte to fill with
-
  * @n: number of spaces to fill
-
  *
-
  * Return: pointer to memory area s
-
  */
 
 char *_memset(char *s, char b, unsigned int n)
