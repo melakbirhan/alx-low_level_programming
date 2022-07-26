5cb13e7a754ee491e7a8afb6da3dec0615d77b9f
melakbirhan <melakbirhan@cbe.com.et>
2022-07-26 10:23:54 +0300

    git changed

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -54,28 +54,32 @@ char **strtow(char *str)
 	words = malloc((total_words + 1) * sizeof(char *));
 	if (words == 0)
 		return (NULL);
-	for (; *str != '\0' &&  b < total_words;) {
+	for (; *str != '\0' &&  b < total_words;)
+	{
 		if (*str == ' ')
 			str++;
 		else
 		{
 			found_word = str;
-			for (; *str != ' ' && *str != '\0';){
+			for (; *str != ' ' && *str != '\0';)
+			{
 				length++;
-				str++; }
+				str++;
+			}
 			words[b] = malloc((length + 1) * sizeof(char));
-			if (words[b] == 0){
+			if (words[b] == 0)
+			{
 				free_everything(words, b);
-				return (NULL); }
-			while (*found_word != ' ' && *found_word != '\0'){
+				return (NULL);
+			}
+			while (*found_word != ' ' && *found_word != '\0')
+			{
 				words[b][c] = *found_word;
-				found_word++;
-				c++; }
+				found_word++; c++;
+			}
 			words[b][c] = '\0';
-			b++;
-			c = 0;
-			length = 0;
-			str++;
-		} }
+			b++; c = 0; length = 0; str++;
+		}
+	}
 	return (words);
 }
