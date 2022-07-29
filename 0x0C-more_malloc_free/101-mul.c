92cda1b1d725156d6865e908f0d770cdab9975d6
melakbirhan <melakbirhan@cbe.com.et>
2022-07-29 12:31:29 +0300

    file added

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -12,16 +12,20 @@ int main(int argc, char *argv[])
 unsigned long mul;
 int i, j;
 
-       if (argc != 3)
-	{ printf("Error\n");
-	exit(98); }
+if (argc != 3)
+	{ 
+		printf("Error\n");
+	exit(98);
+	}
 	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
 			if (argv[i][j] > 57 || argv[i][j] < 48)
-			{  printf("Error\n");
-			exit(98); }
+			{
+				printf("Error\n");
+			exit(98);
+			}
 		}
 
 	}
@@ -29,4 +33,3 @@ int i, j;
 	printf("%lu\n", mul);
 return (0);
 }
-
