12299714bb7002e15c60c873fc8c994c189c9d4d
melakbirhan <melakbirhan@cbe.com.et>
2022-08-03 12:49:45 +0300

    0-print_name.c is added

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,7 +1,11 @@
-#include<stdio.h>  
-void main()  
+#include "function_pointers.h"
+/**
+  * print_name - prints a name
+  * @name: input string
+  * @f: function pointer casted to char pointer
+  */
+void print_name(char *name, void (*f)(char *))
 {
-  clrscr();  
-  printf("My Name is melak");    
-  getch();  
+	if (name != NULL && f != NULL)
+		f(name);
 }
