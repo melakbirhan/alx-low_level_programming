a55c847a701fe6d4cf14af950cd0db362af4bd91
melakbirhan <melakbirhan@cbe.com.et>
2022-08-22 14:55:34 +0300

    1-create_file.c file changed

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -4,40 +4,47 @@
 #include "main.h"
 
 /**
- * append_text_to_file - apends text to file
+ * create_file - append to file
  * @filename: path to file
  * @text_content: content
- * Return: 1 or -1
+ * Return: chars read
  */
-int append_text_to_file(const char *filename, char *text_content)
+
+int create_file(const char *filename, char *text_content)
 {
 	int fd;
 	ssize_t w;
 	int size;
+	char *mem;
 
 	if (!filename)
+	{
 		return (-1);
-
-	fd = open(filename, O_WRONLY | O_APPEND);
-
+	}
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
-
 	if (!text_content)
 	{
 		close(fd);
 		return (1);
 	}
-
 	size = _strlen(text_content);
-	w = write(fd, text_content, size);
-
-	if (w == -1)
+	mem = malloc(sizeof(char) * size);
+	if (!mem)
 	{
 		close(fd);
 		return (-1);
 	}
+	w = write(fd, text_content, size);
+	if (w == -1)
+	{
+		close(fd);
+		free(mem);
+		return (-1);
+	}
 	close(fd);
+	free(mem);
 	return (1);
 }
 
